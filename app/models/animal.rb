39c5c7f89aa1380a15193c8ba912764489941886
Audrey MarieAnne <marieanneaudrey@gmail.com>
2021-12-20 16:55:48 -0400

    Configurer les messages pour afficher les requetes pour le propriétaire

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,6 +1,7 @@
 class Animal < ApplicationRecord
   SPECIES = ["Chien", "Chat", "Oiseau", "Rongeur", "Autre"]
   belongs_to :user
+  has_many :demands
   validates :name, :species, :race, :location, presence: true
   has_many_attached :photos
 end
