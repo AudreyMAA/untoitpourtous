0f2420944bf64b29c5d3b90954136f600086d7b6
Audrey MarieAnne <marieanneaudrey@gmail.com>
2021-12-20 12:24:37 -0400

    Made modifs in search bar and demands

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :animals
+  has_many :demands
   validates :first_name, :last_name, :email, presence: true
 end
